186b33c6d984d350c3f1d2c121321585e71e063f
chrisandrewca <chris.andrew.ca@gmail.com>
2021-02-07 19:31:08 -0800

    Exploring merge sort with printf

--- merge-sort.c
+++ merge-sort.c
@@ -32,20 +32,16 @@ void do_merge_sort(size_t starti, size_t row_size, int row[]) {
 
 		size_t middlei = (starti + row_size) / 2;
 
-		printf("%zu\n", middlei);
 		do_merge_sort(starti, middlei, row);
-
-		//printf("left %zu\n", middlei);
 
 		do_merge_sort(middlei + 1, row_size, row);
 
-		//printf("right %zu\n", middlei + 1);
-
 		merge(starti, middlei, row_size, row);
-	} else {
-		printf("pop - starti: %zu - %zu - row size: %zu\n", starti, (starti + row_size) / 2, row_size);
 	}
 }
 
 void merge(size_t starti, size_t middlei, size_t row_size, int row[]) {
+	printf("%zu\t%zu\t%zu\n", starti, middlei, row_size);
+
+	printf("%zu\t%zu\t%zu\n\n", middlei - starti + 1, row_size - middlei, row_size - starti);
 }
