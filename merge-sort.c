0b4524ffbe044672d296a83d73bc65274d05aa8a
chrisandrewca <chris.andrew.ca@gmail.com>
2021-03-03 09:46:02 -0800

    It finally works! MeRgE sOrT

--- merge-sort.c
+++ merge-sort.c
@@ -4,104 +4,141 @@
 #include "stdlib.h"
 #include "merge-sort.h"
 
-void do_merge_sort(int p, int r, int row[]);
-void merge(int p, int q, int r, int row[]);
+void do_merge_sort(size_t p, size_t r, int row[]);
+void merge(size_t p, size_t q, size_t r, int row[]);
 
 void merge_sort(size_t row_size, int row[]) {
 
+	// todo safety checks
+
+	printf("unsorted\n");
 	for (size_t i = 0; i < row_size; i++) {
 		printf("%d ", row[i]);
 	} printf("\n");
 
-	do_merge_sort(0, row_size, row);
+	do_merge_sort(0, row_size - 1, row);
 
-	printf("\n");
+	printf("\nsorted\n");
 	for (size_t i = 0; i < row_size; i++) {
 		printf("%d ", row[i]);
 	} printf("\n");
 }
 
-// recursive calls are a LIFO stack
-// think about applicability for search algorithms on graphs
-void do_merge_sort(int p, int r, int row[]) {
+void do_merge_sort(size_t p, size_t r, int row[]) {
+
 	if (p < r) {
-		size_t q = (p + r) / 2;
+		int q = (p + r) / 2;
+		//printf("q: %d\n", q);
+		//size_t q = p + (r - p) / 2;
 
 		do_merge_sort(p, q, row);
-
 		do_merge_sort(q + 1, r, row);
-
 		merge(p, q, r, row);
 	}
 }
 
-void merge(int p, int q, int r, int row[]) {
-	assert(p <= q);
-	assert(q < r);
+void merge(size_t p, size_t q, size_t r, int row[]) {
 
-	// assert(q - p + 1 > 0);
-	assert(q - p >= 0);
-	assert(r - p >= 1);
+	size_t n1 = q - p + 1;
+	size_t n2 = r - q;
 
-	// assert(p + 0 - 1 > -2); // ?
-	assert(p + 0 >= 0);
-	assert(q + 0 >= 0);
+	int *scratch = malloc((n1 + n2) * sizeof(int));
 
-	// q++;
+	size_t i = 0, j = 0, k = 0;
 
-	size_t length = r - p;
-	int *copy = malloc(length * sizeof(int));
+	while (i < n1 && j < n2) {
 
-	int *left = NULL, *right = NULL;
-	size_t pp = p, qq = q;
-
-	printf("p: %d, p2: %d, q: %d, q2: %d, r: %d, length: %d \n",
-		p,
-		q - p,
-		q,
-		r - q,
-		r,
-		r - p);
-
-	for (size_t n = 0; n < length; n++) {
-		if (p < qq) { // up to qq?
-			left = &row[p];
+		if (row[p + i] <= row[q + 1 + j]) {
+			scratch[k] = row[p + i];
+			i++;
 		} else {
-			left = NULL;
+			scratch[k] = row[q + 1 + j];
+			j++;
 		}
 
-		if (q < r) {
-			right = &row[q];
-		} else {
-			right = NULL;
-		}
-
-		if (left != NULL && right != NULL) {
-			if (*left <= *right) {
-				copy[n] = *left;
-				p++;
-			} else {
-				copy[n] = *right;
-				q++;
-			}
-		} else if (left != NULL) {
-			copy[n] = *left;
-			p++;
-		} else if (right != NULL) {
-			copy[n] = *right;
-			q++;
-		} else {
-			printf(" undefined loop \n");
-		}
+		k++;
 	}
 
-	for (size_t n = pp, k = 0; k < length; k++) {
-		row[n + k] = copy[k];
+	while (i < n1) {
+
+		scratch[k] = row[p + i];
+		i++;
+		k++;
 	}
 
-	// r is not a limit - its the LAST element
-	// printf("p: %zu, r: %zu - ", p, r);
-	// printf("q: %zu, r: %zu\n", q, r);
+	while (j < n2) {
 
-	free(copy);
+		scratch[k] = row[q + 1 + j];
+		j++;
+		k++;
+	}
+
+	for (size_t k = p, i = 0; i < n1 + n2; k++, i++) {
+		row[k] = scratch[i];
+	}
 }
+
+// recursive calls are a LIFO stack
+// think about applicability for search algorithms on graphs
+
+// programviz.com
+// void do_merge_sort(int p, int r, int row[]);
+// void merge(int p, int q, int r, int row[]);
+// void do_merge_sort(int p, int r, int row[]) {
+
+// 	if (p < r) {
+
+// 		int q = p + (r - p) / 2;
+
+// 		do_merge_sort(p, q, row);
+
+// 		do_merge_sort(q + 1, r, row);
+
+// 		merge(p, q, r, row);
+// 	}
+// }
+
+// void merge(int p, int q, int r, int row[]) {
+
+// 	int n1 = q - p + 1;
+// 	int n2 = r - q;
+
+// 	int *L = malloc(n1 * sizeof(int));
+// 	int *R = malloc(n2 * sizeof(int));
+
+// 	for (int i = 0; i < n1; i++) {
+// 		L[i] = row[p + i];
+// 	}
+
+// 	for (int j = 0; j < n2; j++) {
+// 		R[j] = row[q + 1 + j];
+// 	}
+
+// 	int i = 0, j = 0;
+// 	int k = p;
+// 	while (i < n1 && j <n2) {
+// 		if (L[i] <= R[j]) {
+// 			row[k] = L[i];
+// 			i++;
+// 		} else {
+// 			row[k] = R[j];
+// 			j++;
+// 		}
+// 		k++;
+// 	}
+
+// 	while (i < n1) {
+// 		row[k] = L[i];
+// 		i++;
+// 		k++;
+// 	}
+
+// 	while (j < n2) {
+// 		row[k] = R[j];
+// 		j++;
+// 		k++;
+// 	}
+
+// 	free(L);
+// 	free(R);
+// }
